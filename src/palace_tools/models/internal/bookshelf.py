61411c93ef706877b64df248a8f3ced0c1b2a6f4
Jonathan Green <jonathan@razzard.com>
2024-03-06 19:30:49 -0400

    Update the fetch-lcp command to have two subcommands. One to fetch the manifest and one to fetch the underlying LCP file and lcpl license file.

--- src/palace_tools/models/internal/bookshelf.py
+++ src/palace_tools/models/internal/bookshelf.py
@@ -18,13 +18,15 @@ def print_bookshelf_summary(bookshelf: OPDS2Feed) -> None:
 
     print("\n", "  Loans:" if loans else "  No loans.", sep="")
     for p in loans:
-        print(f"\n    {p.metadata.title}")
+        print(f"\n    {p.metadata.title}  ({p.metadata.author.name})")
         for link in p.acquisition_links:
             print(f"      Fulfillment url: {link.href}")
             for indirect in (
                 lnk for lnk in link.indirect_acquisition_links if lnk.get("type")
             ):
                 print(f"        Indirect type: {indirect['type']}")
+            if hashed_pw := link.properties.get("lcp_hashed_passphrase"):
+                print(f"      LCP hashed passphrase: {hashed_pw}")
     print("\n", "  Holds:" if holds else "  No holds.", sep="")
     for p in holds:
         print(f"    {p.metadata.title}")
